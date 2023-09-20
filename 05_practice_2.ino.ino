841a092567b34a7cd44e1dc9f3f320ad37890aab
dockja <106766461+dockja@users.noreply.github.com>
2023-09-20 16:09:19 +0900

    Add files via upload

---
+++ 05_practice_2.ino.ino
@@ -0,0 +1,39 @@
+#define PIN_LED 7
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200); //Initialize serial port
+  while (!Serial) {
+    ; // wait for serial port to connect
+  }
+}
+
+void loop() {
+  digitalWrite(PIN_LED, 0);
+  delay(1000);
+  count=0;
+  while(1) {
+    digitalWrite(PIN_LED, 0);
+    delay(100);
+    count +1;
+    digitalWrite(PIN_LED, 1);
+    delay(100);
+    count = count +1;
+     if (count == 9) {
+      break;
+     }else {
+     continue;
+  }
+  }
+  while(1) {
+    digitalWrite(PIN_LED, 1);
+  }
+}
+/*int toggle_state(int toggle) {
+  if (toggle == 1){
+  toggle = 0;
+  }else{ 
+    toggle = 1;}
+  return toggle;
+}*/
